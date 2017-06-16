622e03b8dcaad9bcb3c265e97ff5aca71b16f6fb
Martin Sehnoutka <msehnout@redhat.com>
2017-06-16 12:58:13 +0200

    Download and build dnssec-trigger from sources

--- setup.sh
+++ setup.sh
@@ -16,15 +16,32 @@ function installPackage {
     done;
 }
 
-installPackage fish nc tmux bind unbound bind-utils rng-tools
+# Install necessary packages
+installPackage fish nc tmux bind unbound bind-utils rng-tools git clang clang-analyzer libcmocka-devel
+# We are going to generate a lot of key pairs
 runCommand 'rngd -r /dev/urandom' 'Entropy sources'
+# Set global debug option for bind
 echo 'OPTIONS="-d 5"' >> /etc/sysconfig/named
+# Allow IP forwarding between network namespaces
 echo 1 > /proc/sys/net/ipv4/ip_forward
+# Install python framework for dnssec testing
 pushd /vagrant/python
 runCommand './setup.py install' 'Install python framework'
 popd 
+# Set up DNS Servers for testing purposes
 runCommand 'dnssec-testing-setup' 'Set up DNS servers and resolvers' 
+# Check that the resolver works
 dnssec-testing-test-unsecure
 dig +dnssec @100.99.1.2 example.com
+
+# Clone dnssec-trigger repository and build from sources
+mkdir /dnssec
+pushd /dnssec
+runCommand 'git clone https://github.com/msehnout/dnssec-trigger-fedora.git .' 'Clone dnssec-trigger repository into local dir'
+runCommand 'dnf builddep dnssec-trigger.spec -y' 'Install dependencies'
+runCommand './configure --with-forward-zones-support --with-hooks=networkmanager' 'Configure'
+# Run unit testing
+make test
+popd
 
 exit 0
